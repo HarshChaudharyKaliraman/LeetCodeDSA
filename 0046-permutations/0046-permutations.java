33a4ff0486933b9c1c8aac59f4d3e0ae9e64f1b5
Harsh Chaudhary <149260243+HarshChaudharyKaliraman@users.noreply.github.com>
2026-07-13 11:56:37 +0530

    Time: 1 ms (87.28%), Space: 45.6 MB (46.61%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,33 +1,24 @@
-class Solution 
-{
-    List<List<Integer>> ans = new ArrayList<>();
+class Solution {
+    List<List<Integer>> ans=new ArrayList<>();
     List<Integer> temp = new ArrayList<>();
-
-    public List<List<Integer>> permute(int[] nums) 
-    {
-      boolean vis[] = new boolean[nums.length];
-      solve(vis, nums);
-      return ans;
+    public List<List<Integer>> permute(int[] nums) {
+        boolean[] vis = new boolean[nums.length];
+        solve(vis, nums);
+        return ans;
     }
-    void solve(boolean vis[], int[] nums)
-    {
-        if(temp.size() == nums.length)
-        {
-            ans.add(new ArrayList<>(temp));
+    void solve(boolean[] vis, int[] nums){
+        if(temp.size() == nums.length){
+            ans.add(new ArrayList(temp));
             return;
         }
-
-        for(int i = 0; i<nums.length; i++)
-        {
-            if(vis[i] ==true)
-               continue;
-
-             vis[i] = true;
+        for(int i=0; i<nums.length; i++){
+            if(vis[i]==true){
+                continue;
+            }
+            vis[i]=true;
             temp.add(nums[i]);
-            
             solve(vis, nums);
-           
-            vis[i] = false;
+            vis[i]=false;
             temp.remove(temp.size()-1);
         }
     }
