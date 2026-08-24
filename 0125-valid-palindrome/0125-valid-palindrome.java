8450e568e739f1d0373d585b656c3c132fa71f79
Harsh Chaudhary <149260243+HarshChaudharyKaliraman@users.noreply.github.com>
2026-08-24 18:37:19 +0530

    Time: 5 ms (37.05%), Space: 46.8 MB (15.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        Deque<Character> d=new ArrayDeque<>();
+        Deque<Character> d = new ArrayDeque<>();
         s=s.toLowerCase();
-        for(char c:s.toCharArray()){
-            if(c>='a' && c<='z'|| (c >= '0' && c <= '9')){
-                d.addFirst(c);
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)>='a' && s.charAt(i)<='z' || s.charAt(i)>='0' && s.charAt(i)<='9'){
+                d.addFirst(s.charAt(i));
             }
         }
-
         while(d.size()>1){
-            if(d.getFirst() != d.getLast()){
+            if(d.getFirst()!=d.getLast()){
                 return false;
             }
             d.removeFirst();
