1dc52365fa2189db271e0bd616b9f2c18d3ea271
Harsh Chaudhary <149260243+HarshChaudharyKaliraman@users.noreply.github.com>
2026-09-23 16:52:25 +0530

    Time: 5 ms (63.14%), Space: 44 MB (97.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,13 +4,11 @@ class Solution {
             return false;
         }
         int freq[] = new int[26];
-         
         for(int i=0; i<s.length(); i++){
             freq[s.charAt(i)-'a']++;
             freq[t.charAt(i)-'a']--;
         }
-
-        for(int i:freq){
+        for(int i : freq){
             if(i!=0){
                 return false;
             }
